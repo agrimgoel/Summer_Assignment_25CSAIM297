d523279941dc753e46410b0fb3ab331e0692a229
agrimgoel <agrimgoel3>
2026-06-23 20:16:34 +0530

    Day 23 Assignment is uploaded

---
+++ Day 23/Q92.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[100];
+    int freq[256] = {0};
+    int i, max = 0;
+    char result;
+    printf("Enter a string: ");
+    scanf("%s", str);
+    for (i = 0; str[i] != '\0'; i++) {
+        freq[(unsigned char)str[i]]++;
+        if (freq[(unsigned char)str[i]] > max) {
+            max = freq[(unsigned char)str[i]];
+            result = str[i];
+        }
+    }
+    printf("Maximum occurring character: %c\n", result);
+    printf("Frequency: %d\n", max);
+    return 0;
+}
